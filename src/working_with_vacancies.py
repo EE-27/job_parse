08bdf81feda732752cbb7d1b30b7a1306bcefa7d
JL34 <johnny2791@seznam.cz>
2023-10-04 14:19:02 +0200

    made abstract method for add_job into json file, i made it so json file rewrites itself, so no need to add him to git

--- src/working_with_vacancies.py
+++ src/working_with_vacancies.py
@@ -1,9 +1,28 @@
 from load_api import *
+import json
+from abc import ABC, abstractmethod
+
+
+class JobDataHandler_abs(ABC):
+    @abstractmethod
+    def add_job(self, job):
+        pass
+
+
+class Json_JobDataHandler(JobDataHandler_abs):
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def add_job(self, job):
+        with open(self.file_path, "a", encoding='utf-8') as file:
+            json.dump(job.__dict__, file, ensure_ascii=False)
+            file.write('\n')
 
 
 class JobOffer:
 
-    def __init__(self, name, url, requirement, responsibility, salary_from=None, salary_to=None, salary_currency=None):
+    def __init__(self, name,job_id, url, requirement, responsibility, salary_from=None, salary_to=None, salary_currency=None):
+        self.job_id = job_id
         self.name = name
         self.salary_from = salary_from
         self.salary_to = salary_to
@@ -19,10 +38,12 @@ class JobOffer:
 hh_api = HeadHunter_API()
 loading(hh_api)
 var = hh_api.data
-
+###
+print(var)
 jobs = []
 
 for item in var["items"]:
+    job_id = item["id"]
     name = item["name"]
     url = item["alternate_url"]
     requirement = item["snippet"]["requirement"]
@@ -37,10 +58,20 @@ for item in var["items"]:
         salary_to = None
         salary_currency = None
 
-    job = JobOffer(name, url, requirement, responsibility, salary_from, salary_to, salary_currency)
+    job = JobOffer(name,job_id, url, requirement, responsibility, salary_from, salary_to, salary_currency)
     jobs.append(job)
 
 jobs_with_salary = []
+
+job_handle = Json_JobDataHandler("jobs.json")
+
+#  tohle maže vše v jobs.json, takže nemusím řešit duplicity
+with open('jobs.json', 'w', encoding='utf-8'):
+    pass
+
+for job in jobs:
+    job_handle.add_job(job)
+
 
 def maximum():
     for job in jobs:
@@ -52,4 +83,6 @@ def maximum():
         print(f"The job with the highest salary is: {max_job.name} - {max_job.salary_from} {max_job.salary_currency}")
     else:
         print("No jobs with salary information found.")
+
+###
 maximum()
